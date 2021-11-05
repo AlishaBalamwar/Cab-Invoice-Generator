08894870df07308db5ab08e43c75cc5a8935280f
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-05 20:46:59 +0530

    Added: logic for multiple rides

---
+++ src/main/java/com/bridgelabz/invoicegenerator/Ride.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.invoicegenerator;
+
+/**
+ * This is separate ride class
+ */
+public class Ride {
+    public double distance;
+    public final int time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
