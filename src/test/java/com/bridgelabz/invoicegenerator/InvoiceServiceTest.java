81100316d9c07198feddc56cbea27e1a8402d4cf
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-05 20:49:51 +0530

    Added: Testcase for multiple ride totalfare

--- src/test/java/com/bridgelabz/invoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/invoicegenerator/InvoiceServiceTest.java
@@ -15,11 +15,21 @@ public class InvoiceServiceTest {
     }
 
     @Test
-    public void givenLessDistanceOrTime_ShouldReturnMinFare(){
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5.0, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
     }
 }
