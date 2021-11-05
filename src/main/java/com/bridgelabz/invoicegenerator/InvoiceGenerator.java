733d210e995d234132ba2a527084e3d6cca7d4b3
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-05 20:19:14 +0530

    Added: Logic for minimum fare

--- src/main/java/com/bridgelabz/invoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/invoicegenerator/InvoiceGenerator.java
@@ -7,6 +7,7 @@ public class InvoiceGenerator {
 
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE =5 ;
 
     /**
      * @param distance-> Getting distance from testcase
@@ -14,6 +15,9 @@ public class InvoiceGenerator {
      * @return-> Returns the total fare
      */
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
