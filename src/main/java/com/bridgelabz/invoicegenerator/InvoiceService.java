8c56dbb9473859d1249736c636ca1a88d1766b20
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-07 23:21:04 +0530

    Added: methods for Invoice summary

--- src/main/java/com/bridgelabz/invoicegenerator/InvoiceService.java
+++ src/main/java/com/bridgelabz/invoicegenerator/InvoiceService.java
@@ -8,6 +8,11 @@ public class InvoiceService {
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_FARE = 5;
+    private RideRepository rideRepository;
+
+    public InvoiceService() {
+        this.rideRepository = new RideRepository();
+    }
 
     /**
      * @param distance-> Getting distance from testcase
@@ -31,5 +36,13 @@ public class InvoiceService {
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
         return new InvoiceSummary(rides.length, totalFare);
+    }
+
+    public void addRides(String userId, Ride[] rides) {
+        rideRepository.addRides(userId, rides);
+    }
+
+    public InvoiceSummary getInvoiceSummary(String userId) {
+        return this.calculateFare(rideRepository.getRides(userId));
     }
 }
