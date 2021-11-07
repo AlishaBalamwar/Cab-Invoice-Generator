71aacb9d26ecac8f13847561638c54d1bf35d241
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-11-07 23:24:47 +0530

    Added: Ride repository to maintain rides of user

--- src/main/java/com/bridgelabz/invoicegenerator/RideRepository.java
+++ src/main/java/com/bridgelabz/invoicegenerator/RideRepository.java
@@ -6,20 +6,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RideRepository {
-    Map<String , ArrayList<Ride>> userRide = null;
+    Map<String, ArrayList<Ride>> userRide = null;
 
-    public RideRepository(){
+    public RideRepository() {
         this.userRide = new HashMap<>();
     }
 
-    public  void addRides(String userId, Ride[] rides){
+    public void addRides(String userId, Ride[] rides) {
         ArrayList<Ride> rideList = this.userRide.get(userId);
-        if(rideList == null){
+        if (rideList == null) {
             this.userRide.put(userId, new ArrayList<>(Arrays.asList(rides)));
         }
     }
 
-    public Ride[] getRides(String userId){
+    public Ride[] getRides(String userId) {
         return this.userRide.get(userId).toArray(new Ride[0]);
     }
 }
